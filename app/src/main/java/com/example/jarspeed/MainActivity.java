2bbbcf82207c84bc5b0c15cee8591de52fd35343
Jules Blanchard <jules.blanchard@iut-rodez.fr>
2023-12-07 16:21:56 +0100

    ADD Register screen UI UPDATE gradle version for more compatibility

--- app/src/main/java/com/example/jarspeed/MainActivity.java
+++ app/src/main/java/com/example/jarspeed/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.jarspeed;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -23,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
         emailEditText.setHintTextColor(getResources().getColor(R.color.gray)); // Use ContextCompat.getColor if you support devices below API level 23
         passwordEditText.setHintTextColor(getResources().getColor(R.color.gray)); // Use ContextCompat.getColor if you support devices below API level 23
 
+        TextView registerTextView = findViewById(R.id.registerPrompt);
+        registerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }

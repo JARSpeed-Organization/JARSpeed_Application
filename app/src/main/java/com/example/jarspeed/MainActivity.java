c07cb3ca0bf0bcacc6bf08d64495a5fd1cf635b5
Jules Blanchard <jules.blanchard@iut-rodez.fr>
2023-12-12 15:54:01 +0100

    CORRECTION Night-mode graphic bug

--- app/src/main/java/com/example/jarspeed/MainActivity.java
+++ app/src/main/java/com/example/jarspeed/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jarspeed;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
         EditText emailEditText = findViewById(R.id.email);
         EditText passwordEditText = findViewById(R.id.password);
 

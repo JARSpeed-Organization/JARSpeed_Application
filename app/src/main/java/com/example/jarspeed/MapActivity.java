85a777202c7f30be6bc79fc929d724db9e5adac2
Jules Blanchard <jules.blanchard@iut-rodez.fr>
2023-12-14 13:30:34 +0100

    SUCCESS ADD map OSM with fake coordinates. REMOVE Marker

--- app/src/main/java/com/example/jarspeed/MapActivity.java
+++ app/src/main/java/com/example/jarspeed/MapActivity.java
@@ -1,11 +1,13 @@
 package com.example.jarspeed;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -28,20 +30,17 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-
+        // Initialisation de la configuration d'OSMdroid
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         mapView = findViewById(R.id.map);
         mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
-
+        GeoPoint startPoint = new GeoPoint(44.3610252, 2.5701567);
         mapController = mapView.getController();
         mapController.setZoom(18.0);
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            initializeMapLocation();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
+        mapController.setCenter(startPoint);
 
         ImageView profileButton = findViewById(R.id.imageViewProfile);
         profileButton.setOnClickListener(new View.OnClickListener() {
@@ -54,37 +53,4 @@ public class MapActivity extends AppCompatActivity {
         });
     }
 
-    private void initializeMapLocation() {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (location != null) {
-            GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
-            mapController.setCenter(startPoint);
-
-            Marker startMarker = new Marker(mapView);
-            startMarker.setPosition(startPoint);
-            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-            mapView.getOverlays().add(startMarker);
-
-            mapView.invalidate(); // Rafraîchir la carte
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            initializeMapLocation();
-        }
-    }
 }

604aec8823399f9fb376557417433c468c0bb0b3
BLANCHARD JULES <jules.blanchard@open-groupe.com>
2023-12-27 15:04:24 +0100

    START API Connection.

--- app/src/main/java/fr/iutrodez/jarspeed/MainActivity.java
+++ app/src/main/java/fr/iutrodez/jarspeed/MainActivity.java
@@ -5,13 +5,28 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.jarspeed.R;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fr.iutrodez.jarspeed.api.ApiConfig;
+import fr.iutrodez.jarspeed.api.RequestHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +34,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-        EditText emailEditText = findViewById(R.id.email);
-        EditText passwordEditText = findViewById(R.id.password);
+        emailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
 
         emailEditText.setHintTextColor(ContextCompat.getColor(this, R.color.gray));
         passwordEditText.setHintTextColor(ContextCompat.getColor(this, R.color.gray));
@@ -32,9 +47,75 @@ public class MainActivity extends AppCompatActivity {
         startActivity(registerIntent);
     }
 
-    // Méthode pour la gestion du clic sur le bouton de connexion
     public void onLoginClick(View view) {
-        // Démarrez MapActivity ici
+        attemptLogin();
+    }
+
+    private void attemptLogin() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if (validateInputs(email, password)) {
+            sendLoginRequest(email, password);
+        }
+    }
+
+    private boolean validateInputs(String email, String password) {
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Les champs ne peuvent pas être vides", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void sendLoginRequest(String email, String password) {
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("email", email);
+            postData.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, ApiConfig.LOGIN_URL, postData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        handleLoginSuccess(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Erreur lors de la connexion", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        RequestHelper.getInstance(this).addToRequestQueue(jsonObjectRequest);
+    }
+
+    private void handleLoginSuccess(JSONObject response) {
+        try {
+            // Extraire le JWT de la réponse
+            String token = response.getString("token"); // Assurez-vous que la clé "token" correspond à la clé utilisée dans votre réponse JSON
+
+            // Stocker le JWT dans SharedPreferences
+            SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(getString(R.string.saved_token), token);
+            editor.apply();
+
+            // Naviguer vers la MapActivity
+            navigateToMapActivity();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Erreur lors de l'extraction du token", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private void navigateToMapActivity() {
         Intent mapIntent = new Intent(this, MapActivity.class);
         startActivity(mapIntent);
     }
---
+++ app/src/main/java/fr/iutrodez/jarspeed/api/ApiConfig.java
@@ -0,0 +1,6 @@
+package fr.iutrodez.jarspeed.api;
+
+public class ApiConfig {
+    public static final String BASE_URL = "http://74.x.x.x:8080/api/"; // mettre notre API à nous
+    public static final String LOGIN_URL = BASE_URL + "auth/token"; // URL pour la connexion
+}

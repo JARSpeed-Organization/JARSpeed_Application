b6c1f05a2ffa6d2ed7c39e2e9367f65f7c19d9f5
aVeRySsWood <launay.simon@outlook.com>
2024-02-01 14:14:48 +0100

    First commit to set up layout and classes

---
+++ app/src/main/java/fr/iutrodez/jarspeed/ui/AllCoursesActivity.java
@@ -0,0 +1,22 @@
+package fr.iutrodez.jarspeed.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jarspeed.R;
+
+public class AllCoursesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_courses);
+
+
+
+    }
+
+}
